482bec52a797196f38e680a3bfe2e2a06d363dbc
Rajendra Maharjan <3464136+RajendraMaharjan@users.noreply.github.com>
2023-04-23 21:47:51 -0500

    initial commit for rs management

---
+++ src/main/java/com/miu/rsmanagementbackendapi/RsManagementBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.miu.rsmanagementbackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsManagementBackendApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsManagementBackendApiApplication.class, args);
+    }
+
+}
